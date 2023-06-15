4455ed591d4746199ab7847004d181f2fd4c135f
Mugerwa96 <mugerwaobadiah96@gmail.com>
2023-06-15 19:05:58 +0300

    asyn

--- src/features/moviesSlice.js
+++ src/features/moviesSlice.js
@@ -4,14 +4,14 @@ const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 
 
 export const moviesFetch = createAsyncThunk("movies/moviesFetch",
-   (search) =>
-      axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=2a909aea&type=movie&s=${search ? search : 'x-men'}`)
+  async (search) =>
+     await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=2a909aea&type=movie&s=${search ? search : 'x-men'}`)
          .then(res => res.data.Search)
 )
 
 export const moviesFetchById = createAsyncThunk("movies/moviesFetchById",
-   (id) =>
-      axios.get(`https://www.omdbapi.com/?i=${id}&apikey=2a909aea`)
+  async (id) =>
+   await   axios.get(`https://www.omdbapi.com/?i=${id}&apikey=2a909aea`)
          .then(res => res.data)
 )
 
